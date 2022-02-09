2682078a96be7eb723c78241814aecfd2c5ba2e6
Peter Ahlgren <ahlgren1234@gmail.com>
2022-02-09 13:32:21 +0100

    Initial Commit

---
+++ Dark-Light-Mode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dark-Light-Mode
+//
+//  Created by Peter Ahlgren on 2022-02-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dark-Light-Mode/Dark_Light_ModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dark_Light_ModeApp.swift
+//  Dark-Light-Mode
+//
+//  Created by Peter Ahlgren on 2022-02-09.
+//
+
+import SwiftUI
+
+@main
+struct Dark_Light_ModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
